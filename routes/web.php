51f1cf94448c1e6151ea66cc3d5d148027d3098d
santani423 <santani423@gmail.com>
2025-04-18 04:43:42 +0700

    penyesuaian url home

--- routes/web.php
+++ routes/web.php
@@ -21,11 +21,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get('/home', [HomeController::class, 'index'])->name('home');  
+Route::get('/', [HomeController::class, 'index'])->name('home');  
 Route::resource('paket', PaketTurUmumController::class);
 Route::resource('ruang-media', RuangMediaController::class);
 Route::get('/about-as', [HomeController::class, 'abautAs'])->name('about.as');  
